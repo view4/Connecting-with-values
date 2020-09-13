44b30d5e369162b68cd868df87de529a4d6d7a9e
view4 <39835978+view4@users.noreply.github.com>
2020-09-14 01:02:15 +0300

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,12 @@
 
   var firebaseConfig = {
-    apiKey: "AIzaSyCjdYZQNpA-o8XEtnuFDN6DXmqVuGdE4QE",
-    authDomain: "relating-teachings.firebaseapp.com",
-    databaseURL: "https://relating-teachings.firebaseio.com",
-    projectId: "relating-teachings",
-    storageBucket: "relating-teachings.appspot.com",
-    messagingSenderId: "356910087563",
-    appId: "1:356910087563:web:05a33fb9e6980e09"
+    apiKey: "**********************************",
+    authDomain: "*****************",
+    databaseURL: "****************************",
+    projectId: "*******************************",
+    storageBucket: "********************************",
+    messagingSenderId: "******************",
+    appId: "*****************************"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
